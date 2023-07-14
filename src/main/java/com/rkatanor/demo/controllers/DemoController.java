1982b3ce417826484c11a224f5909f54e9b3f5f6
Rajkumar Katanor <katanor.raj@gmail.com>
2023-07-13 22:52:52 -0400

    added new endpoint

---
+++ src/main/java/com/rkatanor/demo/controllers/DemoController.java
@@ -0,0 +1,13 @@
+package com.rkatanor.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @GetMapping("/name")
+    public String getName(){
+        return "hello world";
+    }
+}
